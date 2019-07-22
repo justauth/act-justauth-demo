673baba7bdad9396288cf14509b52b63c853f965
Yangkai.Shen <237497819@qq.com>
2019-07-22 23:05:19 +0800

    :sparkles: act framework 集成 JustAuth 完成

--- src/main/java/com/xkcoding/demo/AppEntry.java
+++ src/main/java/com/xkcoding/demo/AppEntry.java
@@ -16,8 +16,8 @@ import org.osgl.mvc.annotation.GetAction;
 public class AppEntry {
 
     @GetAction("/test")
-    public void hello(H.Response resp) {
-        resp.writeText("hello, act-framework!");
+    public void hello(H.Response response) {
+        response.writeText("hello, act-framework!");
     }
 
     public static void main(String[] args) throws Exception {
--- src/main/java/com/xkcoding/demo/action/OAuthAction.java
+++ src/main/java/com/xkcoding/demo/action/OAuthAction.java
@@ -1,6 +1,21 @@
 package com.xkcoding.demo.action;
 
-import act.controller.annotation.UrlContext;
+import act.Act;
+import act.conf.AppConfig;
+import cn.hutool.core.lang.Dict;
+import me.zhyd.oauth.config.AuthConfig;
+import me.zhyd.oauth.config.AuthSource;
+import me.zhyd.oauth.model.AuthCallback;
+import me.zhyd.oauth.model.AuthResponse;
+import me.zhyd.oauth.request.AuthGithubRequest;
+import me.zhyd.oauth.request.AuthMiRequest;
+import me.zhyd.oauth.request.AuthQqRequest;
+import me.zhyd.oauth.request.AuthRequest;
+import me.zhyd.oauth.utils.AuthState;
+import org.osgl.$;
+import org.osgl.http.H;
+import org.osgl.mvc.annotation.GetAction;
+import org.osgl.mvc.annotation.ResponseContentType;
 
 /**
  * <p>
@@ -10,11 +25,81 @@ import act.controller.annotation.UrlContext;
  * @author yangkai.shen
  * @date Created in 2019-07-22 18:30
  */
-@UrlContext("/oauth")
 @SuppressWarnings("unused")
 public class OAuthAction {
 
-    public void login(){
+    /**
+     * 登录类型
+     */
+    @GetAction("/oauth")
+    @ResponseContentType(H.MediaType.JSON)
+    public Dict loginType(H.Response response) {
+        return Dict.create()
+                .set("QQ登录", "http://oauth.xkcoding.com/demo/oauth/login/qq")
+                .set("GitHub登录", "http://oauth.xkcoding.com/demo/oauth/login/github")
+                .set("小米登录", "http://oauth.xkcoding.com/demo/oauth/login/mi");
+    }
 
+    /**
+     * 认证页面
+     *
+     * @param source   来源 {@link AuthSource}
+     * @param response response
+     */
+    @GetAction("/oauth/login/{source}")
+    public void renderAuth(AuthSource source, H.Response response) {
+        AuthRequest authRequest = getAuthRequest(source);
+        response.sendRedirect(authRequest.authorize());
+    }
+
+    /**
+     * 登录成功后的回调
+     *
+     * @param source   来源 {@link AuthSource}
+     * @param callback 携带返回的信息 {@link AuthCallback}
+     * @return 登录成功后的信息
+     */
+    @GetAction("/oauth/{source}/callback")
+    @ResponseContentType(H.MediaType.JSON)
+    public AuthResponse login(AuthSource source, AuthCallback callback) {
+        AuthRequest authRequest = getAuthRequest(source);
+        AuthResponse response = authRequest.login(callback);
+        // 移除校验通过的state
+        AuthState.delete(source);
+        return response;
+    }
+
+    /**
+     * 获取AuthRequest
+     *
+     * @param source 来源 {@link AuthSource}
+     * @return {@link AuthRequest}
+     */
+    private AuthRequest getAuthRequest(AuthSource source) {
+        AuthConfig config = getAuthConfig(source);
+        switch (source) {
+            case QQ:
+                return new AuthQqRequest(config);
+            case GITHUB:
+                return new AuthGithubRequest(config);
+            case MI:
+                return new AuthMiRequest(config);
+            default:
+                throw new RuntimeException("暂不支持的第三方登录");
+        }
+    }
+
+    private AuthConfig getAuthConfig(AuthSource source) {
+        AppConfig appConfig = Act.appConfig();
+        String type = source.name().toLowerCase();
+        String clientId = $.convert(appConfig.get("oauth." + type + ".client-id")).toString();
+        String clientSecret = $.convert(appConfig.get("oauth." + type + ".client-secret")).toString();
+        String redirectUri = $.convert(appConfig.get("oauth." + type + ".redirect-uri")).toString();
+        return AuthConfig.builder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .redirectUri(redirectUri)
+                .state(AuthState.create(source))
+                .build();
     }
 }
