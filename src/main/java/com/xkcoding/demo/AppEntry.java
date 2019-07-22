bb2d1fa695783f2d731bcef2c2b54b3ac4642a5f
Yangkai.Shen <237497819@qq.com>
2019-07-22 18:19:32 +0800

    :sparkles: hello act framework

---
+++ src/main/java/com/xkcoding/demo/AppEntry.java
@@ -0,0 +1,26 @@
+package com.xkcoding.demo;
+
+import act.Act;
+import org.osgl.http.H;
+import org.osgl.mvc.annotation.GetAction;
+
+/**
+ * <p>
+ * Act 程序入口
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019-07-22 17:36
+ */
+@SuppressWarnings("unused")
+public class AppEntry {
+
+    @GetAction("/test")
+    public void hello(H.Response resp) {
+        resp.writeText("hello, act-framework!");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Act.start("act-justauth-demo");
+    }
+}
