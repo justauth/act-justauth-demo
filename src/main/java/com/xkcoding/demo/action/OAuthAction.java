52321e3d7c5874a529cf22af804e244e34184b7d
Yangkai.Shen <237497819@qq.com>
2019-07-22 20:15:01 +0800

    :bug: 修复 JustAuth lombok 依赖引入问题

---
+++ src/main/java/com/xkcoding/demo/action/OAuthAction.java
@@ -0,0 +1,20 @@
+package com.xkcoding.demo.action;
+
+import act.controller.annotation.UrlContext;
+
+/**
+ * <p>
+ * 第三方登录请求处理
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019-07-22 18:30
+ */
+@UrlContext("/oauth")
+@SuppressWarnings("unused")
+public class OAuthAction {
+
+    public void login(){
+
+    }
+}
